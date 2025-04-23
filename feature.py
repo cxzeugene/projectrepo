44275f5a088bb041e0bb4573ee4e0eeed94dc10b
Eugene <eugenechanxz@gmail.com>
2025-04-24 01:45:39 +0800

    created a python file that has a function that adds 2 numbers. Replace your desired numbers in the file.

---
+++ feature.py
@@ -0,0 +1,9 @@
+# add 2 numbers
+def add_num (firstNum, secondNum):
+    return firstNum + secondNum
+
+first = 10
+second = 20
+result = add_num(first, second)
+
+print(f"Sum of {first} and {second} is {result}")
